0768f20d23ecb089108cb0fbf8cdd568e18d2413
Daniel A. Cifuentes C <dacifuentes@outlook.com>
2023-06-02 20:28:08 -0500

    :construction: Repo init

---
+++ next.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    customKey: 'customValue',
+  },
+  /* basePath: '/dist', */
+  // Comenta la linea basePath para evitar el error 404
+  compress: true,
+  async redirects() {
+    return [
+      {
+        source : '/hola',
+        destination: 'https://github.com/D4rkPK', // IMPORTANTE, el link del destino sin /
+        permanent: true,                          // ya que nos dira que no puede encontrar la ruta
+      }
+    ]
+  }
+}
+
+module.exports = nextConfig
